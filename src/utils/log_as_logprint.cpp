4362b5c1a4408f6670dc7e0afc8a59d249ab4a1f
SitePi <weibo@routerplus.com>
2026-01-01 00:46:55 +0800

    修复 log_as_logprint.cpp 使用正确的 LOG_REDEMPTION_INTERNAL_IMPL 实现

--- src/utils/log_as_logprint.cpp
+++ src/utils/log_as_logprint.cpp
@@ -1,55 +1,77 @@
 /*
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-Product name: redemption, a FLOSS RDP proxy
-Copyright (C) Wallix 2010-2024
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 2 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program; if not, write to the Free Software
+*   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+*   Product name: redemption, a FLOSS RDP proxy
+*   Copyright (C) Wallix 2010-2016
+*   Author(s): Jonathan Poelen
 */
 
-#include <cstdio>
-#include <ctime>
+#include "cxx/diagnostic.hpp"
+#include "cxx/compiler_version.hpp"
+REDEMPTION_DIAGNOSTIC_PUSH()
+#if REDEMPTION_WORKAROUND(REDEMPTION_COMP_CLANG, >= 500)
+    REDEMPTION_DIAGNOSTIC_CLANG_IGNORE("-Wunused-template")
+#endif
+#include "utils/log.hpp"
+REDEMPTION_DIAGNOSTIC_POP()
+
+#ifdef __EMSCRIPTEN__
+# include "red_emscripten/em_asm.hpp"
+#endif
+
 #include <cstdarg>
+#include <cstdio>
 
 #ifdef REDEMPTION_LOG_PRINT_WITH_TIMESTAMP
-#include <sys/time.h>
+#include <ctime>
 #endif
 
-extern "C" {
 
-void LOG_REDEMPTION(int priority, const char* format, ...)
+void LOG_REDEMPTION_INTERNAL_IMPL(int priority, char const * format, ...) noexcept /*NOLINT(cert-dcl50-cpp)*/
 {
+    (void)priority;
+    va_list ap;
+    va_start(ap, format);
+
+    REDEMPTION_DIAGNOSTIC_PUSH()
+    REDEMPTION_DIAGNOSTIC_GCC_IGNORE("-Wformat-nonliteral")
+#ifdef __EMSCRIPTEN__
+    char buffer[1024];
+    int len = std::vsnprintf(buffer, sizeof(buffer), format, ap); /*NOLINT*/
+    RED_EM_ASM({
+        const msg = UTF8ToString($1, $2);
+        if (Module.log) {
+            Module.log($0, msg);
+        }
+        else {
+            console.log(msg);
+        }
+    }, priority, buffer, len);
+#else
 #ifdef REDEMPTION_LOG_PRINT_WITH_TIMESTAMP
-    struct timeval tv;
-    gettimeofday(&tv, nullptr);
-    struct tm tm;
-    localtime_r(&tv.tv_sec, &tm);
-    
-    char timestamp[32];
-    snprintf(timestamp, sizeof(timestamp), "%04d-%02d-%02d %02d:%02d:%02d.%06ld",
-             tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
-             tm.tm_hour, tm.tm_min, tm.tm_sec, tv.tv_usec);
-    
-    fprintf(stderr, "[%s] ", timestamp);
+    std::time_t t = std::time(nullptr);
+    char mbstr[100];
+    if (std::strftime(mbstr, sizeof(mbstr), "%b %e %T ", std::localtime(&t))) {
+        std::fprintf(stderr, "%s", mbstr);
+    }
 #endif
+    std::vfprintf(stderr, format, ap); /*NOLINT*/
+    std::fputs("\n", stderr);
+    // std::fflush(stderr);
+#endif
+    REDEMPTION_DIAGNOSTIC_POP()
 
-    va_list args;
-    va_start(args, format);
-    vfprintf(stderr, format, args);
-    va_end(args);
-    
-    fprintf(stderr, "\n");
-    fflush(stderr);
+    va_end(ap);
 }
-
-} // extern "C"
